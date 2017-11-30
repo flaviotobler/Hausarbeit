fc91bcc0fe16dde87ecdff41c4f2cea2cd289582
Flavio Tobler <flavio.to@hispeed.ch>
2017-11-30 15:23:50 +0100

    Offline

--- public/ServiceWorker.js
+++ public/ServiceWorker.js
@@ -1,14 +1,34 @@
 var cacheName = 'seitenaufbau';
 
+
 self.addEventListener('install', event => {
 	event.waitUntil(
 	caches.open(cacheName)
 	.then(cache => cache.addAll([
 		'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css',
 		'./Aufgabenverwaltung.css',
+		'./index.html',
 		'https://code.jquery.com/jquery-3.2.1.slim.min.js',
 		'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js',
 		'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js'
 		]))
 	);
 });
+
+this.addEventListener('fetch', event => {
+	if(event.request.method ==='GET' && (event.request.name == "selectionD" || event.request.name == "selectionT")){
+		event.respondWith(
+			fetch(event.request.url).catch(error => {
+				return caches.match(offlineUrl);
+			})
+		);
+	} else {
+		event.respondWith(
+		caches.match(event.request, {
+			ignoreSearch: true
+		}).then(function(response) {
+			return response || fetch(event.request);
+		})
+	);
+	}
+}
