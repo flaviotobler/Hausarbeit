f0010e9c83dc789321658f6f7885ff0b8ed0a9cb
Flavio Tobler <flavio.to@hispeed.ch>
2017-11-30 16:31:24 +0100

    a

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.post('/add', function (req, res) {
 	tasks.push(newTask);
 	console.log(newTask);
 	console.log(tasks);
-	res.sendFile(JSON.stringify(tasks));
+	res.send(JSON.stringify(tasks));
 });
 
 app.post('/close', function (req, res) {
