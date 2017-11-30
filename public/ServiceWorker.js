3456da6b67f9c0490357aea25e57e83477965436
Flavio Tobler <flavio.to@hispeed.ch>
2017-11-30 18:40:34 +0100

    Signed-off-by: Flavio Tobler <flavio.to@hispeed.ch>

--- public/ServiceWorker.js
+++ public/ServiceWorker.js
@@ -2,7 +2,7 @@ var cacheName = 'seitenaufbau';
 
 
 self.addEventListener('install', event => {
-	/*event.waitUntil(
+	event.waitUntil(
 	caches.open(cacheName)
 	.then(cache => cache.addAll([
 		'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css',
@@ -12,7 +12,7 @@ self.addEventListener('install', event => {
 		'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js',
 		'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js'
 		]))
-	);*/
+	);
 });
 
 self.addEventListener('fetch', event => {
